11ec1e565cc5b5aa1d569ed487bdf6cf2a9ba8b7
Jörg Bächtiger <jorik@creektiger.ch>
2026-06-03 00:05:42 +0200

    Add phone right-axis mapping

--- src/main/java/ch/flighttrack/tracks/PhoneMount.java
+++ src/main/java/ch/flighttrack/tracks/PhoneMount.java
@@ -6,6 +6,11 @@ final class PhoneMount {
     static final double FORWARD_Y = 0.0;
     static final double FORWARD_Z = 0.0;
 
+    static final String RIGHT_AXIS = "device +Y";
+    static final double RIGHT_X = 0.0;
+    static final double RIGHT_Y = 1.0;
+    static final double RIGHT_Z = 0.0;
+
     private PhoneMount() {
     }
 }
