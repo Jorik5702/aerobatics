e0c650444160b96033c0f40a0764f0d689379483
Jörg Bächtiger <jorik@creektiger.ch>
2026-06-02 23:22:13 +0200

    Add phone mount constants

---
+++ src/main/java/ch/flighttrack/tracks/PhoneMount.java
@@ -0,0 +1,11 @@
+package ch.flighttrack.tracks;
+
+final class PhoneMount {
+    static final String FORWARD_AXIS = "device -X";
+    static final double FORWARD_X = -1.0;
+    static final double FORWARD_Y = 0.0;
+    static final double FORWARD_Z = 0.0;
+
+    private PhoneMount() {
+    }
+}
