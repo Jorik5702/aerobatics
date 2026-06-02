90b31c13755763984c26de3187c042affee68cb9
Jörg Bächtiger <jorik@creektiger.ch>
2026-06-02 23:37:57 +0200

    Add optional pitch to track points

--- src/main/java/ch/flighttrack/tracks/TrackPoint.java
+++ src/main/java/ch/flighttrack/tracks/TrackPoint.java
@@ -12,7 +12,8 @@ public record TrackPoint(
         double yMeters,
         double zMeters,
         boolean moving,
-        double headingRadians
+        double headingRadians,
+        double pitchRadians
 ) {
     public TrackPoint(
             long timeNanos,
@@ -28,6 +29,24 @@ public record TrackPoint(
             boolean moving
     ) {
         this(timeNanos, secondsElapsed, latitude, longitude, gpsAltitudeMeters, barometricAltitudeMeters,
-                speedMetersPerSecond, xMeters, yMeters, zMeters, moving, Double.NaN);
+                speedMetersPerSecond, xMeters, yMeters, zMeters, moving, Double.NaN, Double.NaN);
+    }
+
+    public TrackPoint(
+            long timeNanos,
+            double secondsElapsed,
+            double latitude,
+            double longitude,
+            double gpsAltitudeMeters,
+            double barometricAltitudeMeters,
+            double speedMetersPerSecond,
+            double xMeters,
+            double yMeters,
+            double zMeters,
+            boolean moving,
+            double headingRadians
+    ) {
+        this(timeNanos, secondsElapsed, latitude, longitude, gpsAltitudeMeters, barometricAltitudeMeters,
+                speedMetersPerSecond, xMeters, yMeters, zMeters, moving, headingRadians, Double.NaN);
     }
 }
